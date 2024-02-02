079c17e6ff0f91d3f474ff7d518fd78940a0c0e3
venkata.akkala <venkata.akkala@sas.com>
2024-02-03 01:03:18 +0300

    Add landing page and styles

--- lovelace-ui/src/App.js
+++ lovelace-ui/src/App.js
@@ -1,22 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <nav className="Menu-bar">
+          <a href="#home">Home</a>
+          <a href="#about">About</a>
+          <a href="#contact">Who is Ada Lovelace?</a>
+          <a href="#about">Learn more</a>
+          <a href="#about">Talk to us</a></nav>
+        <img src="aol-logo-nobg.png" alt="Ada Lovelace" className="Center-image"/>
+        <h1>Age Of Lovelace</h1>
+        <p className="Caption-text">Discover how you aged through Digital Revolution.</p>
+        <a className="Learn-more" href="">What is Digital Revolution? </a>
+        <button className="Get-started">Take me on this journey 🚀</button>
       </header>
     </div>
   );
